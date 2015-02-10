5d1886ec968afa5b809b5b6038d7911b1fb845ab
dlk <daniel_kish@outlook.com>
2015-02-10 20:41:05 +0400

    1

--- test.cpp
+++ test.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
 #include <array>
-#include <type_traits>
 #include <algorithm>
-#include <functional>
 #include <cassert>
-using namespace std;
 
-template <size_t N>
+template <std::size_t N>
 struct matrix_desc 
 {
-	array<size_t, N> extents;
-	size_t size;
-	array<size_t, N> strides;
+	std::array<std::size_t, N> extents;
+	std::size_t size;
+	std::array<std::size_t, N> strides;
 	
 	matrix_desc() = default;
 	matrix_desc(matrix_desc const &) = default;
@@ -19,59 +15,54 @@ struct matrix_desc
 
 	template <class ... Exts>
 	explicit matrix_desc(Exts ... exts);
-	explicit matrix_desc(array<size_t, N> extents);
+	
+	explicit matrix_desc(std::array<std::size_t, N> extents);
+	
 	template <class ... Idxs>
-	size_t operator()(Idxs ... idxs) const;
+	std::size_t operator()(Idxs ... idxs) const;
 private:
-	// helper
-	size_t compute_size();
+	// helpers
+	std::size_t compute_size();
 	void compute_strides();
 };
 
 
-template <size_t N>
+template <std::size_t N>
 	template <class ... Exts>
 	matrix_desc<N>::matrix_desc(Exts ... exts) 
-		: extents{size_t(exts)...}, size{compute_size()}
+		: extents{std::size_t(exts)...}, size{compute_size()}
 	{
 		static_assert(sizeof...(exts) == N, "Wrong number of indices");
 		compute_strides();
 	}
 
-template <size_t N>
-	matrix_desc<N>::matrix_desc(array<size_t, N> _extents) 
+template <std::size_t N>
+	matrix_desc<N>::matrix_desc(std::array<std::size_t, N> _extents) 
 		: extents(_extents), size{compute_size()}
     {
         compute_strides();
     }
 
-template <size_t N>
+template <std::size_t N>
 	template <class ... Idxs>
-	size_t matrix_desc<N>::operator()(Idxs ... idxs) const
+	std::size_t matrix_desc<N>::operator()(Idxs ... idxs) const
 	{
 		static_assert(sizeof...(idxs) == N, "Wrong number of indices");
-		array<size_t,N> indices{size_t(idxs)...};
-        return inner_product(begin(strides), end(strides), begin(indices), 0u);
+		std::array<std::size_t,N> indices{std::size_t(idxs)...};
+        return std::inner_product(begin(strides), end(strides), begin(indices), 0u);
 	}
 
-template <size_t N>
-size_t matrix_desc<N>::compute_size()
+template <std::size_t N>
+std::size_t matrix_desc<N>::compute_size()
 {
-	multiplies<size_t> mult;
-	return accumulate(begin(extents), end(extents), 1, mult);
+	std::multiplies<std::size_t> mult;
+	return std::accumulate(begin(extents), end(extents), 1, mult);
 }
 
-template <size_t N>
+template <std::size_t N>
 void matrix_desc<N>::compute_strides()
 {
 	strides[N-1] = 1;
 	for (int i = strides.size()-1; i >= 0; --i)
 		strides[i-1] = extents[i] * strides[i];
 }
-
-
-
-
-
-
-
